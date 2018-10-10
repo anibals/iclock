e02908ec094d72ed38fcfe00d41b0a41e09f10de
anibalsibrian <anibalsibrian@192.168.1.25>
2018-10-09 20:13:35 -0600

    ficheros iniciales

---
+++ iclock/src/main/java/iclock/com/iclock/IclockApplication.java
@@ -0,0 +1,12 @@
+package iclock.com.iclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IclockApplication.class, args);
+	}
+}
